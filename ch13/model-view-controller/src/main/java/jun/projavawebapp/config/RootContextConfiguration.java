15a43bb8183c5e38b5f205db61788f1dd7646035
jdcsma <jdcsma@163.com>
2019-12-19 21:06:06 +0800

    add comments and reformat codes.

--- ch13/model-view-controller/src/main/java/jun/projavawebapp/config/RootContextConfiguration.java
+++ ch13/model-view-controller/src/main/java/jun/projavawebapp/config/RootContextConfiguration.java
@@ -20,22 +20,45 @@ public class RootContextConfiguration {
 
     // 实例化 XML 转换器实现。
     @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        logger.traceEntry();
+        /**
+         * Implementation of the GenericMarshaller interface for JAXB 2.2.
+         *
+         * The typical usage will be to set either the "contextPath" or the "classesToBeBound"
+         * property on this bean, possibly customize the marshaller and unmarshaller by setting
+         * properties, schemas, adapters, and listeners, and to refer to it.
+         */
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan(new String[]{"jun.projavawebapp.site.entity"});
+        return logger.traceExit(marshaller);
+    }
+
+    // 实例化 JSON 转换器实现。
+    @Bean
     public ObjectMapper objectMapper() {
         logger.traceEntry();
+        /**
+         * ObjectMapper provides functionality for reading and writing JSON,
+         * either to and from basic POJOs (Plain Old Java Objects), or to and
+         * from a general-purpose JSON Tree Model (JsonNode), as well as related
+         * functionality for performing conversions. It is also highly customizable
+         * to work both with different styles of JSON content, and to support more
+         * advanced Object concepts such as polymorphism and Object identity.
+         *
+         * ObjectMapper also acts as a factory for more advanced ObjectReader and
+         * ObjectWriter classes. Mapper (and ObjectReaders, ObjectWriters it constructs)
+         * will use instances of JsonParser and JsonGenerator for implementing actual
+         * reading/writing of JSON. Note that although most read and write methods are
+         * exposed through this class, some of the functionality is only exposed via
+         * ObjectReader and ObjectWriter: specifically, reading/writing of longer
+         * sequences of values is only available through ObjectReader.readValues(InputStream)
+         * and ObjectWriter.writeValues(OutputStream).
+         */
         ObjectMapper mapper = new ObjectMapper();
         mapper.findAndRegisterModules();
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         mapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
         return logger.traceExit(mapper);
-    }
-
-    // 实例化 JSON 转换器实现。
-    @Bean
-    public Jaxb2Marshaller jaxb2Marshaller() {
-        logger.traceEntry();
-        //
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setPackagesToScan(new String[]{"jun.projavawebapp.site.entity"});
-        return logger.traceExit(marshaller);
     }
 }
