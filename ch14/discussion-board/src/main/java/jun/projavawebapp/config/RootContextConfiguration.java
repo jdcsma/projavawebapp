ce718bf707d71d16d17b9e3e3bfcd1dcd49ce125
jdcsma <jdcsma@163.com>
2019-12-26 13:54:28 +0800

    add async and scheduling supports.

--- ch14/discussion-board/src/main/java/jun/projavawebapp/config/RootContextConfiguration.java
+++ ch14/discussion-board/src/main/java/jun/projavawebapp/config/RootContextConfiguration.java
@@ -8,21 +8,37 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Controller;
+import sun.rmi.runtime.Log;
+
+import java.util.concurrent.Executor;
 
 /**
  * 该类用于配置全局共享的 bean
  */
 @Configuration
+@EnableAsync(proxyTargetClass = true)
+@EnableScheduling
 @ComponentScan(basePackages = {"jun.projavawebapp.site"},
         /**
          * 将同时包含默认包含过滤器和排除过滤器。
         */
         excludeFilters = {@ComponentScan.Filter(Controller.class)})
-public class RootContextConfiguration {
+public class RootContextConfiguration
+        implements AsyncConfigurer, SchedulingConfigurer {
 
     private final static Logger logger = LogManager.getLogger();
+    private final static Logger schedulingLogger =
+            LogManager.getLogger(logger.getName() + ".[scheduling]");
 
     // 实例化 XML 转换器实现。
     @Bean
@@ -66,5 +82,37 @@ public class RootContextConfiguration {
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         mapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
         return logger.traceExit(mapper);
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        logger.info("Setting up thread pool task scheduler with 20 threads.");
+
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(20);
+        scheduler.setThreadNamePrefix("task-");
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.setErrorHandler(t -> schedulingLogger.error(
+                "Unknown error occurred while executing task.", t
+        ));
+        scheduler.setRejectedExecutionHandler((r, e) -> schedulingLogger.error(
+                "Execution of task {} was rejected for unknown reasons.", r
+        ));
+
+        return scheduler;
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        Executor executor = this.taskScheduler();
+        logger.info("Configuring asynchronous method executor {}.", executor);
+        return executor;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        TaskScheduler scheduler = this.taskScheduler();
+        logger.info("Configuring scheduled method executor {}.", scheduler);
+        taskRegistrar.setTaskScheduler(scheduler);
     }
 }
