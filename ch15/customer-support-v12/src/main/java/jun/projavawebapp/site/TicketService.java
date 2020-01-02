1be47572d364c7912011410483d3ef11828934c9
jdcsma <jdcsma@163.com>
2020-01-02 16:20:25 +0800

    add ch15 example projects.

---
+++ ch15/customer-support-v12/src/main/java/jun/projavawebapp/site/TicketService.java
@@ -0,0 +1,10 @@
+package jun.projavawebapp.site;
+
+import java.util.List;
+
+public interface TicketService
+{
+    List<Ticket> getAllTickets();
+    Ticket getTicket(long id);
+    void save(Ticket ticket);
+}
