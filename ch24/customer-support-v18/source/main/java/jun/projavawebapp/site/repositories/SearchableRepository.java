69e1786bac5d9f993cd93812572af8c24a324abd
liujun <jdcsma@163.com>
2020-04-04 16:15:05 +0800

    add customer-support-v18 project.

---
+++ ch24/customer-support-v18/source/main/java/jun/projavawebapp/site/repositories/SearchableRepository.java
@@ -0,0 +1,9 @@
+package jun.projavawebapp.site.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface SearchableRepository<T> {
+    Page<SearchResult<T>> search(String query, boolean useBooleanMode,
+                                 Pageable pageable);
+}
