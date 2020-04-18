c227104ab54bd6de98243bda487616b8ff61f87c
jdcsma <jdcsma@163.com>
2020-04-18 16:34:31 +0800

    reformating code.

--- ch24/advanced-mappings/source/main/java/jun/projavawebapp/site/EverythingService.java
+++ ch24/advanced-mappings/source/main/java/jun/projavawebapp/site/EverythingService.java
@@ -9,24 +9,31 @@ import jun.projavawebapp.site.entities.User;
 
 import java.util.List;
 
-public interface EverythingService
-{
+public interface EverythingService {
+
     void saveApplicant(Applicant applicant);
+
     List<Applicant> getAllApplicants();
+
     Applicant getApplicant(long id);
 
     void saveEmployee(Employee employee);
+
     List<Employee> getAllEmployees();
+
     Employee getEmployee(long id);
 
     void saveNewsArticle(NewsArticle newsArticle);
+
     List<NewsArticle> getAllNewsArticles();
 
     void savePerson(Person person);
+
     List<Person> getAllPersons();
 
     void saveResume(Resume resume);
 
     void saveUser(User user);
+
     List<User> getAllUsers();
 }
